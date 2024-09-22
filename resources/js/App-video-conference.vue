0dda391c658390a62dc379707b36d3d5b618cd12
melantinomalo <melantinomalo@protonmail.com>
2024-09-22 20:25:08 +0330

    seperate rooms and conference views

--- resources/js/App-video-conference.vue
+++ resources/js/App-video-conference.vue
@@ -2,18 +2,29 @@
   <div>
     <p>Video Conference Test</p>
 
-    <VCRooms />
+    <button @click="showRooms = !showRooms ">
+      {{ (!showRooms)? 'Show Rooms' : 'Hide Rooms'}}
+    </button>
+
+    <VCRooms
+        v-if="showRooms"
+    />
 
     <VCRoomJoin
+        v-else
         room-id="66d5d218a77436496c8df8f3"
     />
+
   </div>
 </template>
 
 <script setup>
-  import {inject} from "vue";
+import { ref, inject } from "vue";
 
-  const webrtc = inject('webrtc')
+const webrtc = inject('webrtc')
+
+const showRooms = ref(false)
+
 
 </script>
 
